63a4338074c633b35e87301bd449fb1f064003bd
Peter Helewski <peter.helewski@stelligent.com>
2019-05-28 14:21:34 -0400

    prompt user for password and otp. update output

--- python/main.py
+++ python/main.py
@@ -3,38 +3,49 @@ import json
 from requests.auth import HTTPBasicAuth
 from getpass import getpass
 
+# User prompts
+# Ask the user for their GitHub password
+gh_pw = getpass(prompt="Enter your GitHub Password: ")
+
+# Ask the user for their GitHub OTP
+gh_otp = input("Enter your GitHub One-Time-Password: ")
+
+
 # Search for "test" Token
-first_otp = input("OTP1: ")
 authorizations = requests.get(
     'https://api.github.com/authorizations',
-    auth=HTTPBasicAuth('phelewski', getpass()),
-    headers={'x-github-otp': first_otp}
+    auth=HTTPBasicAuth('phelewski', gh_pw),
+    headers={'x-github-otp': gh_otp}
 )
-# print(authorizations.json())
-response = authorizations.json()
+print("GitHub List of Authorizations:")
+print(authorizations.json())
 
+# Print current ID that matches desired App Name
+response = authorizations.json()
 token_id = None
 for i in response:
     if i['app']['name'] == 'test':
         token_id = i['id']
+print("Token ID: " + str(token_id)) # TODO: prettier
 
-print(token_id) # TODO: prettier
 
 # Delete "test" Token
-second_otp = input("OTP2: ")
 delete_authorization = requests.delete(
     'https://api.github.com/authorizations/' + str(token_id),
-    auth=HTTPBasicAuth('phelewski', getpass()),
-    headers={'x-github-otp': second_otp}
-) #TODO: spit out something useful
+    auth=HTTPBasicAuth('phelewski', gh_pw),
+    headers={'x-github-otp': gh_otp}
+)
+print("Delete Response:")
+print(delete_authorization)
+
 
 # Create new "test" Token
-third_otp = input("OTP3: ")
 new_authorization = requests.post(
     'https://api.github.com/authorizations',
-    auth=HTTPBasicAuth('phelewski', getpass()),
-    headers={'x-github-otp': third_otp},
+    auth=HTTPBasicAuth('phelewski', gh_pw),
+    headers={'x-github-otp': gh_otp},
     data='{"scopes":["public_repo"], "note": "test"}'
 )
+print("New GitHub Token:")
 print(new_authorization.json())
 print(new_authorization.json()['token'])
