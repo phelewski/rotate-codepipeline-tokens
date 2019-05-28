73c3961ddc44ac628306991ba3528032125ecd52
Peter Helewski <peter.helewski@stelligent.com>
2019-05-28 16:23:08 -0400

    split functions into single use

--- python/main.py
+++ python/main.py
@@ -12,9 +12,11 @@ def get_current_tokens(username, password, otp, token):
     )
     print("GitHub List of Authorizations:")
     print(authorizations.json())
+    return authorizations.json()
 
+def get_token_id(username, password, otp, token):
     # Print current ID that matches desired App Name
-    response = authorizations.json()
+    response = get_current_tokens(username, password, otp, token)
     token_id = None
     for i in response:
         if i['app']['name'] == token:
@@ -24,7 +26,7 @@ def get_current_tokens(username, password, otp, token):
 
 def delete_token(username, password, otp, token):
     # Delete "test" Token
-    token_id = get_current_tokens(username, password, otp, token)
+    token_id = get_token_id(username, password, otp, token)
     delete_authorization = requests.delete(
         'https://api.github.com/authorizations/' + str(token_id),
         auth=HTTPBasicAuth(username, password),
@@ -32,6 +34,7 @@ def delete_token(username, password, otp, token):
     )
     print("Delete Response:")
     print(delete_authorization)
+    return delete_authorization
 
 def create_new_token(username, password, otp, token):
     # Create new "test" Token
@@ -44,6 +47,7 @@ def create_new_token(username, password, otp, token):
     print("New GitHub Token:")
     print(new_authorization.json())
     print(new_authorization.json()['token'])
+    return new_authorization.json()
 
 def main():
     # User prompts
@@ -54,6 +58,9 @@ def main():
     
     # Collect all current Tokens
     get_current_tokens(gh_username, gh_pw, gh_otp, gh_token_name)
+
+    # Get defined Token ID
+    get_token_id(gh_username, gh_pw, gh_otp, gh_token_name)
 
     # Delete defined Token
     delete_token(gh_username, gh_pw, gh_otp, gh_token_name)
