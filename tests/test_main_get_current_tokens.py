42ede259e47a2cbf71e341b245625d66fd41913f
Peter Helewski <peter.helewski@stelligent.com>
2019-05-31 10:15:07 -0400

    update mock token response name

--- tests/test_main_get_current_tokens.py
+++ tests/test_main_get_current_tokens.py
@@ -45,7 +45,7 @@ def test_get_current_tokens_status_code_200(
         'id': 123456789,
         'url': 'https://api.github.com/authorizations/123456789',
         'app': {
-            'name': 'foobar',
+            'name': 'qux_token',
             'url': 'https://developer.github.com/v3/oauth_authorizations/',
             'client_id': '00000000000000000000'
         },
@@ -53,7 +53,7 @@ def test_get_current_tokens_status_code_200(
         'hashed_token': \
             '12ab34cd56ef78gh90ij12lm34no56pq78rs90tu12vw34xy56za78bc90de12fg',
         'token_last_eight': '90de12fg',
-        'note': 'foobar',
+        'note': 'qux_token',
         'note_url': None,
         'created_at': '2019-05-30T15:21:24Z',
         'updated_at': '2019-05-30T15:21:24Z',
